36c621391693846e6fbe58da604bffaa6d03d846
andreobriennz <andre@communica.co.nz>
2018-06-18 23:04:51 +1200

    Add gulp files from simple-gulp-setup

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+
+const settings = {
+    dist: 'dist', 
+    styles: [ 
+        'src/normalize.scss',
+        'src/skeleton.scss',
+        'src/flexbox-grid.scss',
+    ],
+}
+
+// CSS/SASS: compile scss to css, autoprefix, minify
+gulp.task('sass', function () {
+    return gulp.src( settings.styles ) 
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({ 
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(rename('skeleton-flexbox.css')) 
+        .pipe(gulp.dest( settings.dist )) 
+        .pipe(rename('skeleton-flexbox.min.css'))
+        .pipe(minify({compatibility: 'ie8'})) 
+        .pipe(gulp.dest( settings.dist ));
+});
+
+// WATCH FOR CHANGES
+gulp.task('watch', function() {
+    gulp.watch( settings.scripts, ['scripts']);
+    gulp.watch( settings.styles, ['sass']);
+});
+
+gulp.task('default', ['sass', 'scripts', 'watch']);
+gulp.task('vendor', ['vendor-sass', 'vendor-scripts']);
